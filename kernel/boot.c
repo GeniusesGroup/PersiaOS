1bf8f36f13a7dcf5698b8a3bfed14370f1eab319
Mohammad Amin Ahmadi <sudoahmadi@gmail.com>
2023-08-24 13:59:21 +0330

    Load kernel into 32-bit protected mode using GRUB

--- kernel/boot.c
+++
@@ -1,1 +0,0 @@
-/* For license and copyright information please see LEGAL file in repository */
--- kernel/main.c
+++ kernel/main.c
@@ -1,5 +1,5 @@
 /* For license and copyright information please see LEGAL file in repository */
 
-int main()
+void main()
 {
 }
